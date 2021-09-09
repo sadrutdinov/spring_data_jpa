6cc2c220ac1a9c962b7428c06deafbf9889c70d4
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-09 14:49:53 +0400

    100. Инlesson терфейс JpaRepository

---
+++ src/main/java/com/sai/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.sai.spring.springboot.spring_data_jpa.dao;
+
+
+
+import com.sai.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByName(String name);
+}
---
+++ src/main/java/com/sai/spring/springboot/spring_data_jpa/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.sai.spring.springboot.spring_data_jpa.service;
+
+
+
+
+
+import com.sai.spring.springboot.spring_data_jpa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+    public List<Employee> findAllByName(String name);
+}
